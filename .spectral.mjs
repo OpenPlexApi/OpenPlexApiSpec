8505826136e4aa796e1c5832a5dea1ccde3db7ad
Jason Landbridge <jasonlandbridge@protonmail.com>
2024-08-02 11:17:32 +0200

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NDQ3NA/spectral.js?branch_slug=master&token=dddd4b70-21ba-44f5-8b5c-eab9e36a879c";
+export default { extends: ruleset };
